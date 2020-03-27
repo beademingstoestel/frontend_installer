68bc7644607a97fd57a2c94dc9c223f106cb3003
TomBruyneel <14891247+TomBruyneel@users.noreply.github.com>
2020-03-27 10:12:40 +0100

    Add installer script for ubuntu 18

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#run with sudo
+apt-get update
+apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io
+
+echo "Run 'docker-compose up' to start the server"
